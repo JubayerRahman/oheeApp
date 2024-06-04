91e9394b9c984e85fce8cee6ccf72e704082dd96
JubayerRahman <jubayerr398@gmail.com>
2024-06-04 14:56:11 +0600

    added FlatList,RefreshControl, Alert, etc

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ This is just a poractive app. I didnt learnet any code for make this app. All i 
 - nativeWind
 - react-native-safe-area-context
 - react-native vector icons
+
+Okay, the app is complete. So far I have learned a lot of things. Obviously, I didn't add everything here. but I think it is a pretty reasonable app. First of all, it has a beautiful layout design. I learned tabs (Which is a navigation method), fetching data, using Flatlist, Alert, Modals, and RefreshControl. Right now I don't have any idea what I should add to it. So It is complete for right now.
--- app/Work.jsx
+++ app/Work.jsx
@@ -1,29 +1,35 @@
-import { ActivityIndicator, Image, Modal, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, ScrollView, Text, View, Alert, FlatList, Image, Modal, TouchableOpacity, RefreshControl } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link } from 'expo-router'
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Link } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 
 const Work = () => {
 
   const [works, setWorks] = useState([])
+  const [loading, setLoading] = useState(true)
   const [modalShow, setModalShow] = useState([])
-  const [imageLoading, setImageLoading] = useState([])
-  const [error, setError] = useState(null)
+  const [refreashMode, setRefreashMode] = useState(false)
 
-  useEffect(()=>{
-    axios.get("https://final-poejects-gannery-server.vercel.app/projects")
-    .then(res=> {
-      setWorks(res.data)
-      setModalShow(new Array(res.data.length).fill(false))  
-      setImageLoading(new Array(res.data.length).fill(true))  
-    })
-    .catch(err=> {
-      setError("Failed to load projects. Please try again later.")
-      console.log(err);
-    })
+  useEffect( ()=>{
+    fetchWorks()
   },[])
+
+  const fetchWorks = ()=>{
+    try {
+      axios.get("https://final-poejects-gannery-server.vercel.app/projects")
+     .then(res=> {
+      setWorks(res.data)
+      setModalShow(new Array(res.data.length).fill(false)) 
+    })
+     setLoading(false)
+    } catch (error) {
+      Alert.alert("Api is not responding, Maybe network issue. Plase try again later")
+      setLoading(true)
+    }
+  }
+
+  // console.log(modalShow);
 
   const toggleModals = (index) =>{
     const updatedModals = [...modalShow]
@@ -31,81 +37,68 @@ const Work = () => {
     setModalShow(updatedModals)
   }
 
-  const handelImageLoadng = (index) =>{
-    const loadingimag = [...imageLoading]
-    loadingimag[index] = false
-    setImageLoading(loadingimag)
+  const refreshFunction = async()=>{
+    setModalShow(true);
+    await fetchWorks()
+    setRefreashMode(false)
   }
 
-  const closeErrorModal = () => {
-    setError(null);
-  }
 
+  // my working boxs !!
+  const renderWorks = ({item, index})=>(
+    <TouchableOpacity className="w-full mb-[20px] p-[20px] rounded-[10px] bg-[#F3F6F6]" onPress={()=> toggleModals(index)}>
+      <Image className="h-[200px] w-full bg-white mb-[20px] rounded-[10px]" source={{uri: item.img}} resizeMode='cover' />
+      <Text className="font-pregular text-[16px]">{item.name}</Text>
+      <Modal  onRequestClose={() => toggleModals(index)} animationType='slide' transparent={true} visible={modalShow[index]}>
+      <ScrollView>
+      <ScrollView  className="h-[500px] w-[95%] mx-auto  mt-[100px] p-[20px]  rounded-[10px] bg-white">
+      <View className="flex-row justify-end">
+      <Text className='bg-red-700 text-white font-pbold text-[14px] w-[30px] h-[30px] text-center p-[5px] rounded-full' onPress={()=>toggleModals(index)}>X</Text>
+      </View>
+            <Text className="text-center text-[30px] mt-[20px] font-pregular">{item.name}</Text>
+            <View className="flex-row justify-evenly mt-[20px]">
+                        <Link className='text-[20px] font-pregular text-gray-500 items-center' href={item.LiveLink}><Icon name='link' size={20}/> Live Link </Link>
+                        <Link className='text-[20px] font-pregular text-gray-500 items-center' href={item.GitRepo}><Icon name='github-alt' size={20}/> Git Repo </Link>
+            </View>
+            <Text className="font-pregular text-[14px] text-gray-600 leading-5 p-[10px]">{item.description}</Text>
+            <Image resizeMode='contain' className=" h-[250px] rounded-2xl  opacity-[80] drop-shadow-2xl"  source={{uri: item.img}} />
+        </ScrollView>
+      </ScrollView>
+      </Modal>
+    </TouchableOpacity>
+  )
 
   return (
-    <ScrollView  className="p-[10px] bg-[#DCDEF3]">
-      <ScrollView className="bg-white w-[95%] p-[20px] rounded-xl flex flex-1 mx-auto mb-[40px] mt-[150px]">
-        
-
-       <View className="flex flex-row justify-start gap-5 items-center">
-            <Text className="font-rmedium text-4xl">Work's</Text>
-            <View className="h-[6px] w-[50%] rounded bg-[#1B74E4]"></View>
-        </View>
-
-        {
-          error && (
-            <Modal transparent={true} animationType='slide' visible={!!error} >
-              <View>
-              <View className="flex-row justify-end">
-                      <Text className='bg-red-700 text-white font-pbold text-[14px] w-[30px] h-[30px] text-center p-[5px] rounded-full' onPress={closeErrorModal}>X</Text>
-                </View>
-                <Text className="font-pregular text-center mt-[20px] text-[20px]">{error}</Text>
-              </View>
-            </Modal>
-          )}
-
-      {
-        works.length>0 ?
-        (
-          works.map((work, index)=>
-            <View key={work._id} className="p-[10px] bg-[#F3F6F6] mb-[10px] rounded-[10px]">
-                <TouchableOpacity onPress={()=> toggleModals(index)} >
-                  {
-                    imageLoading[index] && <ActivityIndicator size="large" color="black"/>
-                  }
-                <Image
-                className={`" h-[250px] rounded-2xl  opacity-[80] drop-shadow-2xl" ${imageLoading[index] ? "hidden": "block"}`}
-                source={{uri: work.img}} resizeMode='cover' onLoad={()=> handelImageLoadng(index)} />
-                <Text className="font-pregular text-[16px] mt-[20px]">{work.name}</Text>
-              </TouchableOpacity>
-              <Modal   onRequestClose={() => toggleModals(index)} animationType='fade' transparent={true} visible={modalShow[index]}>
-                <ScrollView>
-                  <ScrollView  className="h-[500px] w-[95%] mx-auto  mt-[100px] p-[10px]  rounded-[10px] bg-white">
-                    <View className="flex-row justify-end">
-                      <Text className='bg-red-700 text-white font-pbold text-[14px] w-[30px] h-[30px] text-center p-[5px] rounded-full' onPress={()=>toggleModals(index)}>X</Text>
-                    </View>
-                    <Text className="text-center text-[30px] mt-[20px] font-pregular">{work.name}</Text>
-                    <View className="flex-row justify-evenly mt-[20px]">
-                        <Link className='text-[20px] font-pregular text-gray-500 items-center' href={work.LiveLink}><Icon name='link' size={20}/> Live Link </Link>
-                        <Link className='text-[20px] font-pregular text-gray-500 items-center' href={work.GitRepo}><Icon name='github-alt' size={20}/> Git Repo </Link>
-                    </View>
-                    <Text className="font-pregular text-[14px] text-gray-600 leading-5 p-[10px]">{work.description}</Text>
-                    <Image resizeMode='contain' className={`" h-[250px] rounded-2xl  opacity-[80] drop-shadow-2xl" ${imageLoading[index] ? "hidden": "block"}`}  source={{uri: work.img}} onLoad={()=>handelImageLoadng(index)} />
-                  </ScrollView>
-                </ScrollView>
-              </Modal>
+    <View  className="p-[10px] bg-[#DCDEF3] h-full">
+      <View className="bg-white w-[95%]  p-[20px] rounded-xl flex flex-1 mx-auto  mt-[150px]">
+          <View className="flex flex-row justify-start gap-5 items-center">
+                <Text className="font-rmedium text-4xl">Work's</Text>
+                <View className="h-[6px] w-[50%] rounded bg-[#1B74E4]"></View>
             </View>
-          )
-        )
-        :
-        (<ActivityIndicator size="large" color="black"/>)
-      }
-      </ScrollView>
-      <StatusBar hidden={false} barStyle="dark" backgroundColor="#ffffff" />
-    </ScrollView>
+              {
+              works.length  === 0 ? 
+              <View className="flex-1 items-center justify-center">
+                <Image className="w-[90%] h-[200px]" source={require("../assets/loadingWork.gif")} resizeMode='contain'/>
+                <Text className="font-rmedium text-3xl mt-[20px] text-[#1B74E4]">Loading....</Text>
+              </View>
+              :
+              <FlatList
+              className=" mt-[20px] w-full flex-1"
+              data={works}
+              keyExtractor={(item)=> item._id}
+              renderItem={renderWorks}
+              refreshControl={
+                <RefreshControl
+                refreshing={refreashMode}
+                onRefresh={refreshFunction}
+                />
+              }
+              />
+              
+            }
+      </View>
+    </View>
   )
 }
 
 export default Work
-
-const styles = StyleSheet.create({})
